64b5ac7dd4fc81ffe0f33b9dd30f5a21e8055340
pauliea <paul.adam@live.se>
2016-04-05 20:22:10 +0200

    indent

--- program.cs
+++ program.cs
@@ -7,45 +7,45 @@ using System.Threading.Tasks;
 
 namespace ValueTypes
 {
-	
-	public class Program
-	{
-		private const int NumberOfPoints = 10000000;
-		private const int Repititions = 10;
 
-		static void SearchPointInList<TPoint>(string description, Func<int, TPoint> pointMaker)
-			where TPoint : struct
-		{
-			List<TPoint> points = new List<TPoint>();
+    public class Program
+    {
+        private const int NumberOfPoints = 10000000;
+        private const int Repititions = 10;
 
-			for(int i = 0; i < NumberOfPoints; ++i)
-			{
+        static void SearchPointInList<TPoint>(string description, Func<int, TPoint> pointMaker)
+            where TPoint : struct
+        {
+            List<TPoint> points = new List<TPoint>();
+
+            for (int i = 0; i < NumberOfPoints; ++i)
+            {
                 points.Add(pointMaker(i));
-			}
+            }
 
-			TPoint toFind = pointMaker(NumberOfPoints + 1);
-			int gen0CollectionsAtStart = GC.CollectionCount(0);
+            TPoint toFind = pointMaker(NumberOfPoints + 1);
+            int gen0CollectionsAtStart = GC.CollectionCount(0);
 
-			Stopwatch sw = Stopwatch.StartNew();
+            Stopwatch sw = Stopwatch.StartNew();
 
-			for(int i = 0; i < Repititions; ++i)
-			{
-				points.Contains(toFind);
-			}
+            for (int i = 0; i < Repititions; ++i)
+            {
+                points.Contains(toFind);
+            }
 
-			sw.Stop();
+            sw.Stop();
 
             Console.WriteLine($"{description} \n\tAverage time per lookup: {sw.ElapsedMilliseconds / Repititions} ms \n\tGarbage collections: { GC.CollectionCount(0) - gen0CollectionsAtStart } ");
         }
 
-		
-		public void Main()
-		{
+
+        public void Main()
+        {
             SearchPointInList("Naked struct", i => new PointV1 { X = i, Y = i });
             SearchPointInList("With Equals override", i => new PointV2 { X = i, Y = i });
             SearchPointInList("With Equals overload", i => new PointV3 { X = i, Y = i });
             SearchPointInList("With IEquatable", i => new PointV4 { X = i, Y = i });
             SearchPointInList("All", i => new PointV5 { X = i, Y = i });
         }
-	}
+    }
 }
